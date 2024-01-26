70730d97926eda325c1f3331429bc7b1be7b7be7
Lorenzo Virano <lorenzo.virano@meetweb.it>
2024-01-26 19:34:47 +0100

    Added basic style to forms and form pages

--- portfolio-project-client/src/pages/Home.jsx
+++ portfolio-project-client/src/pages/Home.jsx
@@ -5,7 +5,7 @@ export default function Home(props){
     return(
         <>
             <Header />
-            <Banner title="Lorem ipsum" subtitle="Lorem ipsum" image="https://images.unsplash.com/photo-1495745966610-2a67f2297e5e?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+            <Banner title="Lorem ipsum" subtitle="Lorem ipsum" image="https://images.unsplash.com/photo-1495745966610-2a67f2297e5e?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
         </>
     )
 }
--- portfolio-project-client/src/pages/auth/signup/Signup.jsx
+++ portfolio-project-client/src/pages/auth/signup/Signup.jsx
@@ -5,14 +5,35 @@ export default function Signup(props){
     return(
         <>
             <Header />
-            <div className="form__container">
-                <h3>Signup</h3>
-                <form action="#" className="form__login">
-                    <input type="text" className="login__input"/>
-                    <input type="text" className="login__input"/>
-                    <input type="text" className="login__input"/>
-                    <button className="login__submit">Submit</button>
-                </form>
+            <div className="form__wrapper">
+                <div className="form__container">
+                    <form action="#" className="form form__signup">
+                        <h3 className="form__title">Sign up</h3>
+                        <p className="form__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur eos magni odit praesentium quo? Nemo neque nesciunt qui repellat sed.</p>
+                        <div className="form__input signup__input">
+                            <label>Lorem</label>
+                            <input type="text"/>
+                        </div>
+                        <div className="form__input signup__input">
+                            <label>Lorem</label>
+                            <input type="text"/>
+                        </div>
+                        <div className="form__input signup__input">
+                            <label>Lorem</label>
+                            <input type="text"/>
+                        </div>
+
+                        <div className="form__submit signup__submit">
+                            <button className="form__submit__button">Submit</button>
+                        </div>
+                    </form>
+                </div>
+                <div className="form__banner" style={{backgroundImage: `url('https://images.unsplash.com/photo-1495745966610-2a67f2297e5e?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`}}>
+                    <div className="form__banner__inner">
+                        <h4 className="form__banner__title">Lorem ipsum</h4>
+                        <p className="form__banner__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A cumque debitis dolor earum eum fugit ipsum iusto ullam ut voluptatum.</p>
+                    </div>
+                </div>
             </div>
         </>
     )
