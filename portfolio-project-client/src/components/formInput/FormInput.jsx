8fd995e96f0c4cf8ea8743f97282e78129a57d3d
Lorenzo Virano <lorenzo.virano@meetweb.it>
2024-01-28 23:14:24 +0100

    Create validation and handleSubmit for forms (login and signup)

--- portfolio-project-client/src/components/formInput/FormInput.jsx
+++ portfolio-project-client/src/components/formInput/FormInput.jsx
@@ -1,8 +1,23 @@
+import {useState} from "react";
+
 export default function FormInput(props){
+    const [focused, setFocused] = useState(false)
+    const {label, onChange, id, errorMessage, ...inputProps} = props;
+
+    const handleFocus = (e) => {
+        setFocused(true);
+    }
     return (
         <div className="form__input">
-            <label>{props.label}</label>
-            <input type={props.type} placeholder={props.placeholder}/>
+            <label>{label}</label>
+            <input
+                {...inputProps}
+                onChange={onChange}
+                onBlur={handleFocus}
+                onFocus={() => inputProps.name === "confirmPassword" && setFocused(true)}
+                focused={focused.toString()
+            } />
+            <span className="form__input__error">{errorMessage}</span>
         </div>
     )
 }
--- portfolio-project-client/src/pages/auth/login/Login.jsx
+++ portfolio-project-client/src/pages/auth/login/Login.jsx
@@ -1,19 +1,77 @@
 import './Login.css';
 import Header from "../../../components/navbar/Header";
 import FormInput from "../../../components/formInput/FormInput";
+import {useState} from "react";
 
 export default function Login(){
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(values);
+    }
+
+    const onChange = (e) => {
+        setValues({...values, [e.target.name]: e.target.value})
+    }
+
+    const [values, setValues] = useState({
+        username: "",
+        email: "",
+        password: "",
+        confirmPassword: ""
+    });
+
+    const inputs = [
+        {
+            id: 1,
+            name: "username",
+            type: "text",
+            label: "Username",
+            placeholder: "Username",
+            errorMessage: "Lo username deve contenere 3-16 caratteri",
+            pattern: "^[A-Za-z0-9]{3,16}$",
+            required: true
+        },
+        {
+            id: 2,
+            name: "email",
+            type: "email",
+            label: "Email",
+            errorMessage: "Inserisci una mail valida",
+            placeholder: "Email"
+        },
+        {
+            id: 3,
+            name: "password",
+            type: "password",
+            label: "Password",
+            placeholder: "Password",
+            errorMessage: "Lo password deve contenere 8-20 caratteri, un carattere speciale ecc..",
+            pattern:`^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
+            required: true
+        },
+        {
+            id: 4,
+            name: "confirmPassword",
+            type: "password",
+            label: "Conferma Password",
+            placeholder: "Conferma Password",
+            errorMessage: "Le password non coincidono",
+            pattern: values.password,
+            required: true
+        }
+    ]
+
     return(
         <>
             <Header />
             <div className="form__wrapper">
                 <div className="form__container">
-                    <form action="#" className="form form__login">
+                    <form onSubmit={handleSubmit} className="form form__login">
                         <h3 className="form__title">Login</h3>
                         <p className="form__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur eos magni odit praesentium quo? Nemo neque nesciunt qui repellat sed.</p>
-                        <FormInput type={"text"} label={"Lorem"} placeholder={"Lorem"}/>
-                        <FormInput type={"text"} label={"Lorem"} placeholder={"Lorem"}/>
-                        <FormInput type={"text"} label={"Lorem"} placeholder={"Lorem"}/>
+                        {inputs.map((input) => (
+                            <FormInput key={input.id} {...input} value={values[input.name]} onChange={onChange}/>
+                        ))}
                         <div className="form__submit login__submit">
                             <button className="form__submit__button">Submit</button>
                         </div>
